e2252aa9515c7276c2d1c35039f3df9bb953efb7
Pahtrey <pahtrey@gmail.com>
2016-12-13 20:56:59 +0300

    Добавлена тесты для Point

--- lab4/ShapesTests/ShapesTests.cpp
+++ lab4/ShapesTests/ShapesTests.cpp
@@ -1,10 +1,25 @@
 #include "stdafx.h"
 #define BOOST_TEST_NO_MAIN 
 #include <boost/test/included/unit_test.hpp>
+#include "../Shapes/Point.h"
 
-BOOST_AUTO_TEST_SUITE(Shapes)
-	BOOST_AUTO_TEST_CASE(can_run_first_test)
+BOOST_AUTO_TEST_SUITE(Point)
+	BOOST_AUTO_TEST_CASE(can_be_created)
 	{
-		BOOST_CHECK(true);
+		CPoint point(11.11, 5.7);
+		BOOST_CHECK(point.GetX() == 11.11);
+		BOOST_CHECK(point.GetY() == 5.7);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_get_x_value)
+	{
+		CPoint point(11.11, 5.7);
+		BOOST_CHECK(point.GetX() == 11.11);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_get_y_value)
+	{
+		CPoint point(11.11, 5.7);
+		BOOST_CHECK(point.GetY() == 5.7);
 	}
 BOOST_AUTO_TEST_SUITE_END()
