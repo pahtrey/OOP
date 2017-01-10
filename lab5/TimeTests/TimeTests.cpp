edddce8c56d76c3573566c2c8b26fcf168909b6a
Pahtrey <pahtrey@gmail.com>
2017-01-10 16:44:37 +0300

    Добавлены тесты can_be_initialized_by_hours_and_minutes_and_seconds, can_be_initialized_by_timestamp, can_get_hours, can_get_minutes, can_get_seconds, can_work_with_limit_values, can_check_if_wrong_initialized

--- lab5/TimeTests/TimeTests.cpp
+++ lab5/TimeTests/TimeTests.cpp
@@ -8,9 +8,53 @@ struct TimeFixture
 	CTime time;
 };
 
+bool CheckTime(const CTime & time, unsigned hours, unsigned minutes, unsigned seconds)
+{
+	return ((time.GetHours() == hours) && (time.GetMinutes() == minutes) && (time.GetSeconds() == seconds));
+}
+
 BOOST_FIXTURE_TEST_SUITE(Time, TimeFixture)
-	BOOST_AUTO_TEST_CASE(can_run_first_test)
+	BOOST_AUTO_TEST_CASE(can_be_initialized_by_hours_and_minutes_and_seconds)
 	{
-		BOOST_CHECK(true);
+		CTime time(12, 30, 15);
+		BOOST_CHECK(CheckTime(time, 12, 30, 15));
+	}
+
+	BOOST_AUTO_TEST_CASE(can_be_initialized_by_timestamp)
+	{
+		CTime time(36810);
+		BOOST_CHECK(CheckTime(time, 10, 13, 30));
+	}
+
+	BOOST_AUTO_TEST_CASE(can_get_hours)
+	{
+		CTime time(36810);
+		BOOST_CHECK(time.GetHours() == 10);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_get_minutes)
+	{
+		CTime time(36810);
+		BOOST_CHECK(time.GetMinutes() == 13);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_get_seconds)
+	{
+		CTime time(36810);
+		BOOST_CHECK(time.GetSeconds() == 30);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_work_with_limit_values)
+	{
+		CTime time1(86400);
+		CTime time2(0);
+		BOOST_CHECK(CheckTime(time1, 24, 0, 0));
+		BOOST_CHECK(CheckTime(time2, 0, 0, 0));
+	}
+
+	BOOST_AUTO_TEST_CASE(can_check_if_wrong_initialized)
+	{
+		CTime time(99, 32, 83);
+		BOOST_CHECK(!time.isValid());
 	}
 BOOST_AUTO_TEST_SUITE_END()
