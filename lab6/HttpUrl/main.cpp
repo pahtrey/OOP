ea4a32c6051761d3e0d65e9005d802203669b594
Pahtrey <pahtrey@gmail.com>
2017-01-31 20:38:01 +0300

    Добавлена реализация работы со стандартным потоком ввода

--- lab6/HttpUrl/main.cpp
+++ lab6/HttpUrl/main.cpp
@@ -1,7 +1,32 @@
 #include "stdafx.h"
 #include "HttpUrl.h"
+
+using namespace std;
+
 int main()
 {
-	CHttpUrl("http://google.com:333/test page/");
+	string urlString;
+
+	cout << "Input Urls:" << endl;
+
+	while (getline(cin, urlString))
+	{
+		try
+		{
+			CHttpUrl url(urlString);
+
+			cout << "protocol: " << url.ProtocolToString(url.GetProtocol()) << endl;
+			cout << "domain: " << url.GetDomain() << endl;
+			cout << "document: " << url.GetDocument() << endl;
+			cout << "port: " << url.GetPort() << endl;
+			cout << "url: " << url.GetUrl() << endl;
+		}
+		catch (exception const& e)
+		{
+			cout << e.what() << endl;
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
